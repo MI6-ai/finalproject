fbec3ac75e9fec07023db542608348ed0ba352a4
aayushjagwani <ayushja678@gmail.com>
2022-01-09 08:31:56 -0800

    api added

--- src/app/services/news-api-service.service.ts
+++ src/app/services/news-api-service.service.ts
@@ -9,7 +9,7 @@ export class NewsApiServiceService {
 
   constructor(private _http: HttpClient) { }
 
-  NewsApiUrl = "https://newsapi.org/v2/top-headlines?country=in&apiKey=5cc0dece04c446ffb01528372f6cbf5e";
+  NewsApiUrl = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=5cc0dece04c446ffb01528372f6cbf5e";
 
   topHeading(): Observable<any> {
     return this._http.get(this.NewsApiUrl);
