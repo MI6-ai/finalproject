699278af33a7dbc5732435a5d76cb26e475df4ab
aayushjagwani <ayushja678@gmail.com>
2022-01-11 07:27:06 -0800

    ok

--- src/app/services/news-api-service.service.ts
+++ src/app/services/news-api-service.service.ts
@@ -6,13 +6,13 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class NewsApiServiceService {
-  searchquery: string ='iphone';
+  searchquery: string ='mobiles';
 
   constructor(private _http: HttpClient) { }
 
   NewsApiUrl = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=5cc0dece04c446ffb01528372f6cbf5e";
 
-  SearchUrl = "https://newsapi.org/v2/top-headlines?country=in&q=iphone&category=technology&apiKey=5cc0dece04c446ffb01528372f6cbf5e";
+  SearchUrl = "https://newsapi.org/v2/top-headlines?country=in&category=technology&apiKey=5cc0dece04c446ffb01528372f6cbf5e&q="+this.searchquery;
 
   topHeading(): Observable<any> {
     return this._http.get(this.SearchUrl);
